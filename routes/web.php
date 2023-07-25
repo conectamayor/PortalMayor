28a5b9091aaafef19f7973326bc245b1c1e33525
jisjesuscova <jesuscova@jisparking.com>
2023-07-24 20:29:46 -0400

    Cambios en family core datum

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,7 @@ Route::post('/enter', 'LoginController@login');
 Route::get('/', 'HomeController@index');
 Route::get('/login/logout', 'LoginController@logout');
 Route::get('/login/success', 'LoginController@index');
+Route::get('/login/error', 'LoginController@index');
 Route::get('/rut', 'RutController@index');
 Route::get('/rut/error', 'RutController@index');
 Route::post('/rut/verify', 'RutController@verify');
