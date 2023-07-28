d51ae280fad07fdefa40b22f4d5c07f14edbeec7
jisjesuscova <jesuscova@jisparking.com>
2023-07-27 20:37:22 -0400

    cambios

--- resources/js/components/BotonFacebook.vue
+++ resources/js/components/BotonFacebook.vue
@@ -1,12 +1,12 @@
 <template>
-    <button @click="openInstagramProfile">Abrir Instagram</button>
+    <button @click="openWhatsAppApp">Abrir WhatsApp</button>
   </template>
   
   <script>
   export default {
     methods: {
-      openInstagramProfile() {
-        window.open("https://www.instagram.com/tu_perfil_de_instagram", "_blank");
+      openWhatsAppApp() {
+        window.location.href = "whatsapp://"; // Reemplaza el número de teléfono con el número al que deseas enviar un mensaje
       }
     }
   }
