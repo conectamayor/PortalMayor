9cc76b67d7fe273395f480a9ae1dabd26febc1e0
jisjesuscova <jesuscova@jisparking.com>
2023-07-28 07:23:19 -0400

    cambios

--- resources/js/components/BotonFacebook.vue
+++ resources/js/components/BotonFacebook.vue
@@ -6,7 +6,7 @@
   export default {
     methods: {
       openWhatsAppApp() {
-        var destination = "whatsapp://send"; 
+        var destination = "discord://"; 
         if( navigator.userAgent.match(/Android/i) ) {
             // use Android's redirect
             document.location = destination;   
