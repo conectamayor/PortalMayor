bc08714f7dd3a17301de5d81fa2d669d0c503269
jisjesuscova <jesuscova@jisparking.com>
2023-07-27 20:18:27 -0400

    cambios

--- resources/js/components/BotonFacebook.vue
+++ resources/js/components/BotonFacebook.vue
@@ -9,15 +9,10 @@
     methods: {
       openFacebookApp() {
         const urlScheme = 'fb://'; // Facebook URI scheme for opening the app
-        const webUrl = 'https://www.facebook.com'; // Fallback URL to open if the app is not installed
   
         // Try to open the Facebook app
         window.location.href = urlScheme;
-  
-        // If the app is not installed, open the Facebook website in a new tab
-        setTimeout(() => {
-          window.location.href = webUrl;
-        }, 500);
+
       },
     },
   };
