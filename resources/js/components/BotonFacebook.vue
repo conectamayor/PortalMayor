0ec7b05920ac9b56d3331b286117d3fca1c9f6cf
jisjesuscova <jesuscova@jisparking.com>
2023-07-29 15:39:51 -0400

    cambios en el sistema de archivos

--- resources/js/components/BotonFacebook.vue
+++ resources/js/components/BotonFacebook.vue
@@ -1,21 +1,20 @@
+<!-- src/components/MercadoLibreButton.vue -->
 <template>
-    <button @click="openWhatsAppApp">Abrir WhatsApp 2</button>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-      openWhatsAppApp() {
-        var destination = "twitter://"; 
-        if( navigator.userAgent.match(/Android/i) ) {
-            // use Android's redirect
-            document.location = destination;   
-        }   
-        else {
-            // use iOS redirect
-            window.location.replace( destination );
-        }
-      }
-    }
-  }
-  </script>
+  <div>
+    <button @click="openMercadoLibre">Abrir MercadoLibre</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    openMercadoLibre() {
+      // Esquema de URL personalizado para la aplicación de MercadoLibre
+      const mercadoLibreDeepLink = "mercadoLibre://";
+
+      // Intentamos abrir la aplicación de MercadoLibre
+      window.location.href = mercadoLibreDeepLink;
+    },
+  },
+};
+</script>
