4fd38999c5d4e6a990bfe4b9904ddb6c49ac518d
jisjesuscova <jesuscova@jisparking.com>
2023-08-06 20:21:52 -0400

    cambios

--- resources/js/components/CreateContent.vue
+++ resources/js/components/CreateContent.vue
@@ -329,7 +329,6 @@
                 if(this.form.category_id != null
                     && this.form.type_id != null
                     && this.form.google_tag != ''
-                    && this.form.fai != ''
                     && this.form.start_date != ''
                 ) {
                     let formData = new FormData();
