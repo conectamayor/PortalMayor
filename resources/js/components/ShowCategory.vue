543fad404b86098e1e7ef4d627ac404cf3842736
jisjesuscova <jesuscova@jisparking.com>
2023-08-09 09:44:05 -0400

    Cambios

--- resources/js/components/ShowCategory.vue
+++ resources/js/components/ShowCategory.vue
@@ -14,7 +14,16 @@
         </div>
         <div v-if="check_category_poll == 0">
             <div v-if="poll_question_posts == ''" class="row">
-
+                <div class="col-8">
+                    <div class="col-12" v-for="(post, index) in posts" v-bind:index="index">
+                        <router-link v-if="post.icon_available_id == '1'" @click.native="Track(post.google_tag)" class="boton2" :style="{ background: post.color}" :to="`/content/show/${post.content_id}`"> 
+                            <i v-bind:class="post.icon"></i><br> <font class="title">{{ post.title }}</font>
+                        </router-link>
+                        <router-link v-else @click.native="Track(post.google_tag)" class="boton2" :style="{ background: post.color}" :to="`/content/show/${post.content_id}`"> 
+                            <font class="title">{{ post.title }}</font>
+                        </router-link>
+                    </div>
+                </div>
                 <div class="col-4">
                     <div class="circle">
                         <i class="fas fa-chevron-up"></i>
@@ -72,7 +81,16 @@
         </div>
         <div v-if="check_category_poll != 0">
             <div class="row">
-
+                <div class="col-8">
+                    <div class="col-12" v-for="(post, index) in posts" v-bind:index="index">
+                        <router-link v-if="post.icon_available_id == '1'" class="boton2" :style="{ background: post.color}" :to="`/content/show/${post.content_id}`"> 
+                            <i v-bind:class="post.icon"></i><br> {{ post.title }}
+                        </router-link>
+                        <router-link v-else class="boton2" :style="{ background: post.color}" :to="`/content/show/${post.content_id}`"> 
+                            <font class="title">{{ post.title }}</font>
+                        </router-link>
+                    </div>
+                </div>
                 <div class="col-4">
                     <div class="circle">
                         <i class="fas fa-chevron-up"></i>
