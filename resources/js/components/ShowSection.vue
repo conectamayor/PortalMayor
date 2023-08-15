b8554b0c34189bcd03b4e7caafec3b35efff01d1
jisjesuscova <jesuscova@jisparking.com>
2023-08-15 14:13:57 -0400

    cambios

--- resources/js/components/ShowSection.vue
+++ resources/js/components/ShowSection.vue
@@ -335,16 +335,14 @@
 .boton2 {
   font-size: 25px !important;
 }
-
 .circle-container {
     position: absolute;
     top: 50%; /* Alinea el contenedor al centro vertical */
-    left: 90%; /* Ajusta la posición horizontal según tus necesidades */
+    left: 50%; /* Ajusta la posición horizontal según tus necesidades */
     transform: translate(-50%, -50%); /* Centra el contenedor en la pantalla */
 }
 
 .circle {
-    
     width: 60px;
     height: 60px;
     background-color: #3498db;
