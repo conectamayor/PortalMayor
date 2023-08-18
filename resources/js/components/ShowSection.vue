da192f3b392b4a561a64583f36e6b39750edcfa3
jisjesuscova <jesuscova@jisparking.com>
2023-08-18 15:03:45 -0400

    cambios

--- resources/js/components/ShowSection.vue
+++ resources/js/components/ShowSection.vue
@@ -4,13 +4,17 @@
             <hr>
             <h2><center><strong>{{ post.section_subtitle }}</strong></center></h2>
         </div>
-
+        <div v-if="post.video_id != 0" class="row">
+            <h1><center>{{ post.section_title }}</center></h1>
+            <iframe width="560" height="315" :src="`https://www.youtube.com/embed/${post.video_id}?autoplay=1`" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div v-else>
             <div v-if="post.iframe != null && post.iframe != ''" class="row">
                 <h1><center>{{ post.section_title }}</center></h1>
                 <iframe width="600" height="600" :src="`${post.iframe}`" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
 
-
+            <div v-if="check_category_poll == 0">
                 <div v-if="poll_question_posts == ''" class="row">
                     <div class="col-8 col-8-scroll" ref="col8">
                         <div class="p-3 border">
@@ -48,11 +52,17 @@
                     </div>
                     <div class="col-4">
                         <div class="d-flex flex-column align-items-center">
-                            <button @click="scrollContent(-50)" class="btn btn-primary mb-2">Subir</button>
-                            <button @click="scrollContent(50)" class="btn btn-secondary">Bajar</button>
+                            <div class="circle" @click="scrollContent(-50)">
+                                <i class="fas fa-chevron-up"></i>
+                            </div>
+                            <div class="circle" @click="scrollContent(50)">
+                                <i class="fas fa-chevron-down"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
     </div>
 	
 </template>
@@ -241,7 +251,7 @@
 <style>
     /* Agrega estilos para la altura máxima y oculta el scrollbar en la columna de ancho 8 */
     .col-8-scroll {
-      max-height: 500px; /* Ajusta la altura máxima según tus necesidades */
+      max-height: 580px; /* Ajusta la altura máxima según tus necesidades */
       overflow-y: scroll;
       scrollbar-width: none; /* Oculta el scrollbar en navegadores Firefox */
       -ms-overflow-style: none; /* Oculta el scrollbar en navegadores Edge */
