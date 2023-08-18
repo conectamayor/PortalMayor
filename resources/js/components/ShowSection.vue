d2d87a7369aacbe7bdb62cf3a63d9b4520cd3a9f
jisjesuscova <jesuscova@jisparking.com>
2023-08-18 14:45:04 -0400

    cambios

--- resources/js/components/ShowSection.vue
+++ resources/js/components/ShowSection.vue
@@ -60,12 +60,234 @@
     </div>
 	
 </template>
+
 <script>
     export default {
+        created() {
+            this.checkVideo();
+            this.checkCategoryPoll();
+            this.getPollQuestions();
+            this.getPollQuantity();
+            this.getPosts();
+            this.getPolls();
+            this.checkDate();
+
+            localStorage.setItem('circle_position', '10');
+        },
         methods: {
             scrollContent(offset) {
                 const col8 = this.$refs.col8;
                 col8.scrollTop += offset;
+            },
+            scrollUp() {
+                window.scrollBy(0, -50); // You can adjust the scroll amount as needed
+
+                var position = localStorage.getItem('circle_position');
+                
+                const buttonElement = this.$refs.circle_container;
+
+                var newPosition = parseInt(position) - 4;
+
+                console.log(newPosition);
+
+                buttonElement.style.top = `${newPosition}%`;
+
+                localStorage.setItem('circle_position', newPosition);
+            },
+            scrollDown() {
+                window.scrollBy(0, 50); // You can adjust the scroll amount as needed
+                
+                var position = localStorage.getItem('circle_position');
+                
+                const buttonElement = this.$refs.circle_container;
+
+                var newPosition = parseInt(position) + 4;
+
+                console.log(newPosition);
+
+                buttonElement.style.top = `${newPosition}%`;
+
+                localStorage.setItem('circle_position', newPosition);
+
+                var alturaPantalla = window.innerHeight;
+                            var alturaPagina = document.body.scrollHeight;
+                            var posicionScroll = window.scrollY;
+
+                            if (posicionScroll + alturaPantalla >= alturaPagina) {
+                    // Se ha llegado al final de la página
+                    console.log("Se ha llegado al final de la página");
+                    // Aquí puedes detener cualquier animación o acción relacionada con el scroll
+                }
+            },
+            isScrollAtBottom() {
+                const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+                const scrollHeight = Math.max(
+                    document.documentElement.scrollHeight,
+                    document.body.scrollHeight
+                );
+                const clientHeight = document.documentElement.clientHeight;
+
+                return scrollTop + clientHeight >= scrollHeight;
+            },
+            goWeb(url, google_tag) {
+                this.$gtag.event('page_view', {
+                    page_title: google_tag
+                });
+
+                window.location.href = url;
+            },
+            Track(google_tag) {
+                this.$gtag.event('page_view', {
+                    page_title: google_tag
+                });
+            },
+            checkDate() {
+                let formData = new FormData();
+                formData.append('page', 'Home');
+               
+                axios.post('/api/content/date', formData)
+                .then(function (response) {
+                    currentObj.success = response.data.success;
+                })
+                .catch(function (error) {
+                        console.log(error);
+                });
+            },
+            onSubmit(e) {
+                this.loading = true;
+                e.preventDefault();
+                let currentObj = this;
+    
+                const config = {
+                    headers: { 'content-type': 'multipart/form-data' }
+                }
+
+                let formData = new FormData();
+            
+                formData.append('poll_id', this.$route.params.id);
+                formData.append('yes_no_answers', this.form.yes_no_answer);
+                formData.append('text_answers', this.form.text_answer);
+
+                axios.post('/api/poll/answer', formData, config)
+                .then(function (response) {
+                    currentObj.success = response.data.success;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(() => {
+                    this.loading = false;
+                    this.$awn.success("La encuesta han sido contestada", {labels: {success: "Éxito"}});
+                    this.$router.push('/');
+                });
+            },
+            getPollQuantity() {
+                axios.get('/api/poll/quantity/'+ this.$route.params.id)
+                .then(response => {
+                    this.poll_quantity = response.data.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+            },
+            checkCategoryPoll() {
+                axios.get('/api/section/poll/'+ this.$route.params.id)
+                .then(response => {
+                    this.check_category_poll = response.data.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+            },
+            getPolls() {
+                axios.get('/api/poll/all/'+ this.$route.params.id)
+                .then(response => {
+                    this.polls = response.data.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+            },
+            getPollQuestions() {
+                axios.get('/api/poll/show/'+ this.$route.params.id)
+                .then(response => {
+                    this.poll_question_posts = response.data.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+            },
+            catchUser() {
+                let formData = new FormData();
+                formData.append('page', 'section_id_'+this.$route.params.id);
+               
+                axios.post('/api/user/catch', formData)
+                .then(function (response) {
+                    currentObj.success = response.data.success;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            },
+            goHome() {
+                this.$router.push('/')
+            },
+            goBack() {
+                this.$router.go(-1)
+            },
+            checkVideo() {
+                this.loading = true;
+
+                axios.get('/api/section/'+ this.$route.params.id+'/edit')
+                .then(response => {
+                    this.post = response.data.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+            },
+            getPosts() {
+                this.loading = true;
+
+                axios.get('/api/category/show/'+ this.$route.params.id)
+                .then(response => {
+                    this.posts = response.data.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+            }
+        },
+        data: function() {
+            return {
+                posts: [],
+                post: '',
+                polls: [],
+                poll_question_posts: [],
+                poll_quantity: 0,
+                check_category_poll: '',
+                form: {
+                    yes_no_answer: [],
+                    text_answer: []
+                },
             }
         }
     }
