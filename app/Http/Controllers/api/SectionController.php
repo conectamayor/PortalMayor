09f873c0f250bd1e85480ee49d354799d806fd0b
jisjesuscova <jesuscova@jisparking.com>
2023-08-19 22:18:41 -0400

    cambios

--- app/Http/Controllers/api/SectionController.php
+++ app/Http/Controllers/api/SectionController.php
@@ -47,15 +47,17 @@ class SectionController extends ApiResponseController
     public function home(Request $request)
     {
         $sections = Section::select('sections.*')
-            ->leftJoin('section_regions', 'section_regions.section_id', '=', 'sections.section_id')
-            ->leftJoin('communes', 'communes.region_id', '=', 'section_regions.region_id')
-            ->leftJoin('section_communes', 'section_communes.commune_id', '=', 'communes.commune_id')
-            ->where('sections.status', 1)
-            ->where('section_regions.region_id', $request->region)
-            ->where('section_communes.commune_id', $request->commune)
-            ->orderBy('sections.position', 'ASC')
-            ->get();
-        
+                ->distinct() // Agrega la función distinct()
+                ->leftJoin('section_regions', 'section_regions.section_id', '=', 'sections.section_id')
+                ->leftJoin('communes', 'communes.region_id', '=', 'section_regions.region_id')
+                ->leftJoin('section_communes', 'section_communes.commune_id', '=', 'communes.commune_id')
+                ->where('sections.status', 1)
+                ->where('section_regions.region_id', $request->region)
+                ->where('section_communes.commune_id', $request->commune)
+                ->orderBy('sections.position', 'ASC')
+                ->get();
+
+                
         return $this->successResponse($sections);
     }
 
