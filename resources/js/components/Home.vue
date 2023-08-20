bf85de8713be237e41b7aa9e63d682bc1642eb00
jisjesuscova <jesuscova@jisparking.com>
2023-08-19 21:39:17 -0400

    cambios

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -193,7 +193,7 @@
             async getCommune() {
                 try {
                     const response = await axios.get('/api/commune/find');
-
+                    console.log(response.data.data)
                     this.commune = response.data.data.commune_id;
                 } catch (error) {
                     console.log(error);
