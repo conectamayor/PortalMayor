5cf65cad5edc1e939a677ba7c5fd988bc6e6359d
jisjesuscova <jesuscova@jisparking.com>
2023-08-20 17:28:46 -0400

    cambios

--- app/Http/Controllers/api/CategoryCommuneController.php
+++ app/Http/Controllers/api/CategoryCommuneController.php
@@ -29,7 +29,7 @@ class CategoryCommuneController extends ApiResponseController
      */
     public function edit($id)
     {
-        $category_communes = CategoryCommune::where('commune_id', $id)->get();
+        $category_communes = CategoryCommune::where('category_id', $id)->get();
 
         return $this->successResponse($category_communes);
     }
