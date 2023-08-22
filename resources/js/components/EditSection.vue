0c5fc2ef6efb7aa028ddccb4da4fc278c9758123
jisjesuscova <jesuscova@jisparking.com>
2023-08-21 21:27:45 -0400

    cambios en crear seccion

--- resources/js/components/EditSection.vue
+++ resources/js/components/EditSection.vue
@@ -215,7 +215,7 @@
                                     </div>
                                 </div>
 
-                                <div class="form-group row" v-if="form.direct_content_question_id == 2">
+                                <div class="form-group row" v-if="form.direct_content_question_id == 2 && form.youtube_question_id == 2">
                                     <div class="col-sm-4">
                                         <label for="exampleInputEmail1">¿Va a abrir una aplicación? <h6 class="m-0 text-danger float-right">*</h6></label>
                                         <select class="form-control" id="exampleFormControlSelect1"
