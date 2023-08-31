33dc068da606de286e8c1fd91b88c52c528f7c37
jisjesuscova <jesuscova@jisparking.com>
2023-08-31 11:38:46 -0400

    cambios

--- resources/js/components/Menu.vue
+++ resources/js/components/Menu.vue
@@ -8,35 +8,35 @@
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rols_permissions[0]">
+        <li class="nav-item" v-if="rols_permissions[1] || rols_permissions[2] || rols_permissions[3] || rols_permissions[4] || rols_permissions[5] || rols_permissions[6]">
             <router-link class="nav-link" to="/alliance">
             <i class="fas fa-fw fa-handshake"></i>
             <span>Alianzas</span></router-link>
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rols_permissions[1]">
+        <li class="nav-item" v-if="rols_permissions[7] || rols_permissions[8] || rols_permissions[9] || rols_permissions[10]">
             <router-link class="nav-link" to="/section">
             <i class="fas fa-fw fa-arrow-up"></i>
             <span>Secciones</span></router-link>
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rols_permissions[2]">
+        <li class="nav-item" v-if="rols_permissions[11] || rols_permissions[12] || rols_permissions[13] || rols_permissions[14]">
             <router-link class="nav-link" to="/category">
             <i class="fas fa-fw fa-signal"></i>
             <span>Categorias</span></router-link>
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rols_permissions[3]">
+        <li class="nav-item" v-if="rols_permissions[15] || rols_permissions[16] || rols_permissions[17] || rols_permissions[18]">
             <router-link class="nav-link" to="/content">
             <i class="fas fa-fw fa-paint-brush"></i>
             <span>Contenido</span></router-link>
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rols_permissions[4]">
+        <li class="nav-item" v-if="rols_permissions[19] || rols_permissions[20] || rols_permissions[21] || rols_permissions[22] || rols_permissions[23]">
             <router-link class="nav-link" to="/poll">
             <i class="fas fa-fw fa-poll"></i>
             <span>Encuestas</span></router-link>
@@ -50,7 +50,7 @@
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rols_permissions[6]">
+        <li class="nav-item" v-if="rols_permissions[24]">
             <router-link class="nav-link" to="/audit">
             <i class="fas fa-fw fa-thumbs-up"></i>
             <span>Auditoría</span></router-link>
@@ -64,7 +64,7 @@
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rols_permissions[8]">
+        <li class="nav-item" v-if="rols_permissions[25] || rols_permissions[26] || rols_permissions[27] || rols_permissions[28]">
             <router-link class="nav-link" to="/user">
             <i class="fas fa-fw fa-users"></i>
             <span>Usuarios</span></router-link>
