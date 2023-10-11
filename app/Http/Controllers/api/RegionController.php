06032284c3cd90c52d26b547cb0acf7c44f3ced5
jisjesuscova <jesuscova@jisparking.com>
2023-10-11 20:46:05 -0300

    cambios

--- app/Http/Controllers/api/RegionController.php
+++ app/Http/Controllers/api/RegionController.php
@@ -49,7 +49,7 @@ class RegionController extends ApiResponseController
             $ip = $_SERVER['REMOTE_ADDR'];  
         }
 
-        $apiKey = "16823809AF409285AEB1180E176139FF";
+        $apiKey = "D1ADF7A7E66DB897DEF099B7CD05DE34";
 
         $curl = curl_init();
 
